64588d7179fd4bcd24fbcc6feff43ec7ea6280d8
BSCInfos <leoquitto20@gmail.com>
2026-07-14 20:59:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ const ROTACAO_MAPAS = {
             "Bounty": ["Hideout", "Shooting Star", "Layer Cake"],
             "Knockout": ["Goldarm Gulch", "Out in the Open", "New Horizons"]
         }
+        "07": { 
+            "Brawl Ball": ["Pinhole Punt", "Pinball Dreams", "Triple Dribble"], 
+            "Gem Grab": ["Hard Rock Mine", "Crystal Arcade", "Gem Fort"],
+            "Hot Zone": ["Dueling Beetles", "Open Business", "Ring of Fire"],
+            "Heist": ["Safe Zone", "Kaboom Canyon", "Pit Stop"],
+            "Bounty": ["Hideout", "Dry Season", "Layer Cake"],
+            "Knockout": ["Goldarm Gulch", "Out in the Open", "New Horizons"]
+        }
     }
 };
 
