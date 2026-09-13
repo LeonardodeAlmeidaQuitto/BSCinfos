5a03bb1b9a4fdb887cbfbbc270814eacc64a1a5f
BSCInfos <leoquitto20@gmail.com>
2026-09-13 19:29:12 -0300

    Replace 'sk_edu' user with 'acee' user

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
 window.BSC_USERS = [
     {username: "admin",password: "bscinfos"},
     {username: "pique",password: "230711"},
-    {username: "sk_edu",password: "testegratis"},
+    {username: "acee",password: "bscinfos"},
 ];
